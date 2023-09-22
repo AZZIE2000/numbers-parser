841cc3126ea6a707581cd017c4416858576e8260
AZZIE2000 <azzam.faraj0@gmail.com>
2023-09-22 20:11:41 +0300

    tickets inhancment

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
-let input = "is one tickets واحد kids ثلاث and خمس five";
+import membersData from "./membersData";
+
 const extractNumbers = (input: string) => {
   const en_numbersRegex =
     /(zero)|(one|واحد)|(two|اثنان|اثنين)|(three|ثلاث[ه|ة]?)|(four|أربع[ه|ة]?)|(five|خمس[ه|ة]?)|(six|ست[ه|ة]?)|(seven|سبع[ه|ة]?)|(eight|ثماني[ه|ة]?)|(nine|تسع[ه|ة]?)|(ten|عشر[ه|ة]?)/gm;
@@ -26,6 +27,79 @@ const extractNumbers = (input: string) => {
   return input;
 };
 
-console.log(extractNumbers(input));
+// console.log(extractNumbers(input));
 // output: 1 tickets 1 kids 3 and 5 five
 // By Azzam Faraj
+
+const run = (text: string) => {
+  const members = {
+    adults: 0,
+    children: 0,
+    disabled: 0,
+  };
+  const tokenizedText = text.split(" ");
+  for (var i = 0; i < tokenizedText.length; i++) {
+    const word = tokenizedText[i];
+    if (isNaN(parseInt(word)) && tokenizedText[i + 1] !== "for") {
+      if (word === "for") {
+        const ammountNum = !isNaN(parseInt(tokenizedText[i - 1]))
+          ? parseInt(tokenizedText[i - 1])
+          : !isNaN(parseInt(tokenizedText[i - 2]))
+          ? parseInt(tokenizedText[i - 2])
+          : 0;
+        const type = tokenizedText[i + 1];
+        for (var e = 0; e < membersData.length; e++) {
+          const record = membersData[e];
+          if (type?.includes(record.word)) {
+            members[record.type as keyof typeof members] = ammountNum;
+          }
+        }
+      } else if (
+        tokenizedText[i + 1] !== "for" ||
+        tokenizedText[i + 2] !== "for"
+      ) {
+        for (var e = 0; e < membersData.length; e++) {
+          const record = membersData[e];
+          if (word?.includes(record.word)) {
+            const count = isNaN(parseInt(tokenizedText[i - 1]))
+              ? 0
+              : parseInt(tokenizedText[i - 1]);
+            if (members[record.type as keyof typeof members] === 0) {
+              members[record.type as keyof typeof members] =
+                count || record.count;
+            }
+          } else if (!isNaN(parseInt(word))) {
+            const count = parseInt(word);
+            members.adults = count;
+          }
+        }
+      }
+    }
+  
+  }
+  return members;
+};
+
+console.log("-----------------");
+console.log("-----------------");
+console.log("-----------------");
+let input = " two for children and one for an adult";
+console.log(input);
+console.log(run(extractNumbers(input)));
+console.log("-----------------");
+let input11 = " two for children ";
+console.log(input11);
+console.log(run(extractNumbers(input11)));
+console.log("-----------------");
+let input2 = " تذكرتين";
+console.log(input2);
+console.log(run(extractNumbers(input2)));
+console.log("-----------------");
+let input3 = " تذكرة واحدة";
+console.log(input3);
+console.log(run(extractNumbers(input3)));
+console.log("-----------------");
+let input4 = "تذاكز";
+console.log(input4);
+console.log(run(extractNumbers(input4)));
+console.log("-----------------");
---
+++ membersData.ts
@@ -0,0 +1,222 @@
+export default [
+  {
+    word: "طفل",
+    count: 1,
+    type: "children",
+  },
+  {
+    word: "تذكرة",
+    count: 1,
+    type: "adults",
+  },
+  {
+    word: "تذاكر",
+    count: 1,
+    type: "adults",
+  },
+  {
+    word: "تذكرتين",
+    count: 2,
+    type: "adults",
+  },
+  {
+    word: "ticket",
+    count: 1,
+    type: "adults",
+  },
+  {
+    word: "tickets",
+    count: 1,
+    type: "adults",
+  },
+  {
+    word: "طفلين",
+    count: 2,
+    type: "children",
+  },
+  {
+    word: "طفلان",
+    count: 2,
+    type: "children",
+  },
+  {
+    word: "طفال",
+    count: 1,
+    type: "children",
+  },
+  {
+    word: "صغير",
+    count: 1,
+    type: "children",
+  },
+  {
+    word: "صغيرين",
+    count: 2,
+    type: "children",
+  },
+  {
+    word: "صغيران",
+    count: 2,
+    type: "children",
+  },
+  {
+    word: "صغار",
+    count: 1,
+    type: "children",
+  },
+  {
+    word: "كبير",
+    count: 1,
+    type: "adults",
+  },
+  {
+    word: "كبيرين",
+    count: 2,
+    type: "adults",
+  },
+  {
+    word: "كبيران",
+    count: 2,
+    type: "adults",
+  },
+  {
+    word: "كبار",
+    count: 1,
+    type: "adults",
+  },
+  {
+    word: "شخص",
+    count: 1,
+    type: "adults",
+  },
+  {
+    word: "شخصين",
+    count: 2,
+    type: "adults",
+  },
+  {
+    word: "شخصان",
+    count: 2,
+    type: "adults",
+  },
+  {
+    word: "شخاص",
+    count: 1,
+    type: "adults",
+  },
+  {
+    word: "انا",
+    count: 1,
+    type: "adults",
+  },
+  {
+    word: "احد",
+    count: 1,
+    type: "adults",
+  },
+  {
+    word: "person",
+    count: 1,
+    type: "adults",
+  },
+  {
+    word: "people",
+    count: 1,
+    type: "adults",
+  },
+  {
+    word: "child",
+    count: 1,
+    type: "children",
+  },
+  {
+    word: "children",
+    count: 1,
+    type: "children",
+  },
+  {
+    word: "kid",
+    count: 1,
+    type: "children",
+  },
+  {
+    word: "kids",
+    count: 1,
+    type: "children",
+  },
+  {
+    word: "adult",
+    count: 1,
+    type: "adults",
+  },
+  {
+    word: "adults",
+    count: 1,
+    type: "adults",
+  },
+  {
+    word: "child",
+    count: 1,
+    type: "children",
+  },
+  {
+    word: "children",
+    count: 1,
+    type: "children",
+  },
+  {
+    word: "disabled",
+    count: 1,
+    type: "disabled",
+  },
+  {
+    word: "handicap",
+    count: 1,
+    type: "disabled",
+  },
+  {
+    word: "handicapped",
+    count: 1,
+    type: "disabled",
+  },
+  {
+    word: "wheelchair",
+    count: 1,
+    type: "disabled",
+  },
+  {
+    word: "special needs",
+    count: 1,
+    type: "disabled",
+  },
+  {
+    word: "مقعد",
+    count: 1,
+    type: "disabled",
+  },
+  {
+    word: "معاق",
+    count: 1,
+    type: "disabled",
+  },
+  {
+    word: "معاقين",
+    count: 1,
+    type: "disabled",
+  },
+  {
+    word: "معاقان",
+    count: 2,
+    type: "disabled",
+  },
+  {
+    word: "ذوي الاحتياجات الخاصة",
+    count: 1,
+    type: "disabled",
+  },
+  {
+    word: "احتياجات خاصة",
+    count: 1,
+    type: "disabled",
+  },
+];
